1079dfe3d8e2f5575a91855cd0d763dd50f8cf41
Peyton <pekellogg@hotmail.com>
2022-03-21 16:53:53 -0700

    commit tree fa5cc9dcd6c3edb883d8e100a6e5b1b7e0081f7c

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,8 +17,13 @@ class ApplicationController < Sinatra::Base
   end
 
   post "/signup" do
-    #your code here
-
+		user = User.new(:username => params[:username], :password => params[:password])
+		
+    if user.username != "" && user.save
+		  redirect "/login"
+		else
+		  redirect "/failure"
+		end
   end
 
   get '/account' do
@@ -32,7 +37,16 @@ class ApplicationController < Sinatra::Base
   end
 
   post "/login" do
-    ##your code here
+		user = User.find_by(:username => params[:username])
+
+		# user cannot persist w/o PW per ActiveRecord. #save if true after #new
+		#authenticate #=> return User instance if true, else false
+		if user && user.authenticate(params[:password])
+		  session[:user_id] = user.id
+		  redirect "/account"
+		else
+		  redirect "/failure"
+		end
   end
 
   get "/failure" do
